13e65d20bdc18d6194402b5b43ec63347a764a26
Nilton Ginani <nilton.ginani@ccc.ufcg.edu.br>
2018-10-23 16:35:59 -0300

    pegar elementos
    Pegando os produtos e os listando

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 export default class Gallery extends Component {
   render() {
-    // TODO: implement this component
-    return <span>This is the gallery component!</span>;
+    return (
+      <div className="Products">
+        {this.props.products.map(product => {
+          return <li key={product.productId}> {product.productName}</li>;
+        })}
+      </div>
+    );
   }
 }
--- src/components/ProductPrice.js
+++ src/components/ProductPrice.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 export default class ProductPrice extends Component {
+  constructor(props) {
+    super(props);
+    let preco = this.props.products.price;
+  }
   render() {
     // TODO: implement this component
     return null;
