88e4c2649e517419a4a943d75cbd0da8169572a1
Nilton Ginani <nilton.ginani@ccc.ufcg.edu.br>
2018-10-23 17:49:47 -0300

    quase fim

--- src/components/ProductSummary.js
+++ src/components/ProductSummary.js
@@ -5,12 +5,14 @@ import ProductPrice from "./ProductPrice";
 export default class ProductSummary extends Component {
   render() {
     return (
-      <div>
-        <img src={this.props.product.items[0].images[0].imageUrl} />
-        <ProductName name={this.props.product.productName} />
-        <ProductPrice
-          price={this.props.product.items[0].sellers[0].commertialOffer.Price}
-        />
+      <div className="qualquerCoisa">
+        <div className="segundo">
+          <img src={this.props.product.items[0].images[0].imageUrl} />
+          <ProductName name={this.props.product.productName} />
+          <ProductPrice
+            price={this.props.product.items[0].sellers[0].commertialOffer.Price}
+          />
+        </div>
       </div>
     );
   }
